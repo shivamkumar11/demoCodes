b877b67a67d06470cad1e103cfbf2c409bc85f85
shivamkumar11 <shivam991168@gmail.com>
2024-07-24 08:24:55 +0530

    Code send today.

--- src/StrongNo.java
+++ src/StrongNo.java
@@ -4,11 +4,32 @@ public class StrongNo {
     static int temp = 0;
 
     public static void main(String[] args) {
-        //  147
-        Scanner sc =new Scanner(System.in);
+
+        //Creating scanner object here
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter no here: ");
+        //taking input from user here
         String no = sc.next();
+        // passing user input to this method
+        checkStrongNo(no);
+
+    }
+
+    // this method generate factorial of given no and added to the empty temp variable
+    private static void printFactorial(int num) {
+        int fact = 1;
+        int i = 1;
+        while (i <= num) {
+            fact = fact * i;
+            i++;
+        }
+        temp = temp + fact;
+    }
+
+    // here this method perform strong no validation by checking temp value using if else and print strong no or not.
+    private static void checkStrongNo(String no) {
         for (char a : no.toCharArray()) {
+            // calling print factorial here and pass user input here.
             printFactorial(Integer.parseInt(String.valueOf(a)));
         }
         if (temp == Integer.parseInt(no)) {
@@ -17,20 +38,6 @@ public class StrongNo {
             System.out.println(no + " is not strong number");
 
         }
-
-
-    }
-
-    private static void printFactorial(int num) {
-        int fact = 1;
-        int i = 1;
-        while (i <= num) {
-            fact = fact * i;
-            i++;
-        }
-//System.out.println("Factorial of "+num+"= "+fact);
-        temp = temp + fact;
-
     }
 
 
